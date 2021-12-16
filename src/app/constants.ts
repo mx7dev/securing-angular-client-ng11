ebb937aa995a5059b15a412f8cf8ab28bbc1094f
Maximo Carrion <maximo.carrion.dev@gmail.com>
2021-12-16 08:24:09 -0500

    first commit

---
+++ src/app/constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+
+
+	// local
+	public static clientRoot = 'http://localhost:4200/';
+	public static apiRoot = 'https://localhost:2112/api/';
+	public static stsAuthority = 'https://localhost:4242/';
+
+	public static clientId = 'spa-client';
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AccountService } from './account.service';
+import { AdminRouteGuard } from './admin-route-guard';
+import { AuthInterceptorService } from './auth-interceptor.service';
+import { AuthService } from './auth-service.component';
+import { ProjectService } from './project.service';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: [
+        AuthService,
+        AccountService,
+        ProjectService,
+        AdminRouteGuard,
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi:true}
+
+    ],
+})
+export class CoreModule { }
